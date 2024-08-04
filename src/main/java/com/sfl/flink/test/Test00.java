7832d5747fd4d25edd2674614dc1619964577ca9
sun fulong <54763792+simayi0808@users.noreply.github.com>
2024-08-04 23:48:38 +0800

    Update Test00.java

--- src/main/java/com/sfl/flink/test/Test00.java
+++ src/main/java/com/sfl/flink/test/Test00.java
@@ -11,8 +11,7 @@ public class Test00 {
         }
 
         System.out.println(12345);
-        System.out.println(67890);
-        System.out.println(23456);
+        System.out.println(11111);
 
     }
 
