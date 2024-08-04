7f887d42290ccdbae55bfedd56ec23616c6b2cfb
sunFuLong <2446379278@qq.com>
2024-08-04 23:51:46 +0800

    第二版，再将就一下吧

--- src/main/java/com/sfl/flink/test/Test00.java
+++ src/main/java/com/sfl/flink/test/Test00.java
@@ -11,7 +11,7 @@ public class Test00 {
         }
 
         System.out.println(12345);
-        System.out.println(67890);
+        System.out.println(22222);
         System.out.println(23456);
 
     }
